7783d588516f6fe981449163388a5370b1e83248
okorkov <alex.okorkov@gmail.com>
2021-01-14 11:05:13 -0800

    commit tree 811a532c955bbd504eec6a0123de7c3ecce21e9c

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
 end
+
+
+# Add validations to Author such that...
+# The name cannot be blank
+# The e-mail is unique
+# Add validations to Post such that...
+# The title cannot be blank
+# The category is either "Fiction" or "Non-Fiction"
+# The content is at least 100 characters long.
+# Change AuthorsController#create to re-render the form if the new author is invalid.
+# Change PostsController#update to re-render the form if the updated post is invalid.
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :category, acceptance: { accept: ['Non-Fiction', 'Fiction'] }
 end
